4e3ff7434e8f1226819fb9aff26ee27597889b76
Yukari316 <cbgancna@gmail.com>
2022-08-29 19:25:48 +0800

    style: 汉化部分文本(win11)

--- ExplorerPatcher/GUI.c
+++ ExplorerPatcher/GUI.c
@@ -1343,7 +1343,7 @@ static BOOL GUI_Build(HDC hDC, HWND hwnd, POINT pt)
                                         if (GetTimeFormatEx(wszWeatherLanguage[0] ? wszWeatherLanguage : wszLanguage, TIME_NOSECONDS, &stLastUpdate, NULL, wszTime, MAX_PATH))
                                         {
                                             bOk = TRUE;
-                                            swprintf_s(text, MAX_LINE_LENGTH, L"Last updated on: %s, %s.", wszDate, wszTime);
+                                            swprintf_s(text, MAX_LINE_LENGTH, L"上一次更新: %s, %s.", wszDate, wszTime);
                                         }
                                     }
                                 }
@@ -3143,7 +3143,7 @@ static BOOL GUI_Build(HDC hDC, HWND hwnd, POINT pt)
                                 1,
                                 0
                             );
-                            swprintf(text + 3, MAX_LINE_LENGTH, L"Disable per-application window list ( Alt + %c )", key);
+                            swprintf(text + 3, MAX_LINE_LENGTH, L"禁用应用切换列表( Alt + %c )", key);
                         }
                         if (tabOrder == _this->tabOrder)
                         {
