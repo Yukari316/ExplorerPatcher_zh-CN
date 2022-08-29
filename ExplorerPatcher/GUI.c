88e4a9560c0ca1d4b1f9c9d0239269989212ab77
Yukari316 <cbgancna@gmail.com>
2022-08-29 19:47:46 +0800

    fix: 自动生成的字符串支持中文

--- ExplorerPatcher/GUI.c
+++ ExplorerPatcher/GUI.c
@@ -951,6 +951,7 @@ static void GUI_SetSection(GUI* _this, BOOL bCheckEnablement, int dwSection)
 
 static BOOL GUI_Build(HDC hDC, HWND hwnd, POINT pt)
 {
+    setlocale(LC_CTYPE, "zh-CN");
     GUI* _this;
     LONG_PTR ptr = GetWindowLongPtr(hwnd, GWLP_USERDATA);
     _this = (int*)(ptr);
